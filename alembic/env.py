20caa0e38f9f7802a06ad08d8c7e37e18053302c
Bronzin <117386193+Bronzin@users.noreply.github.com>
2025-09-17 16:49:44 +0200

    feat(backend): scaffold Ghigus

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,5 @@
+
+
 """Alembic environment configuration."""
 from __future__ import annotations
 
@@ -7,6 +9,9 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 
 from app.core.config import settings
+
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
